b1810b52bc23acc8381f3401ec5fe6a1d626803b
Abdullah Ahmad <abdullahahmad@example.com>
2026-05-05 00:49:48 +0700

    Server Express pertama berhasil jalan

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000; // Kamu bisa ganti portnya kalau mau
+
+// Baris sakti agar folder public bisa diakses browser
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route utama untuk memanggil index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+app.get('/api/data', (req, res) => {
+    res.json({
+        nama: "Ahmad",
+        nim: "245920109",
+        jurusan: "Teknologi Informasi",
+        kampus: "Universitas Deli Sumatera",
+        status: "Maba"
+    });
+});
+app.listen(PORT, () => {
+    console.log(`Server jalan di http://localhost:${PORT}`);
+});
---
+++ public/js/script.js
@@ -0,0 +1,77 @@
+/* ==================== 1. MENU SHOW & HIDDEN ==================== */
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Tampilkan Menu */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Sembunyikan Menu */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/* ==================== 2. REMOVE MENU MOBILE ==================== */
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/* ==================== 3. SCROLL SECTIONS ACTIVE LINK ==================== */
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if(sectionsClass){
+            if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+                sectionsClass.classList.add('active-link')
+            } else {
+                sectionsClass.classList.remove('active-link')
+            }
+        }                                                    
+    })
+}
+// Jalankan saat scroll
+window.addEventListener('scroll', scrollActive)
+// Jalankan saat REFRESH/RESTART agar element aktif kembali
+window.addEventListener('load', scrollActive)
+
+/* ==================== 4. CHANGE BACKGROUND HEADER ==================== */
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); 
+    else header.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader)
+window.addEventListener('load', scrollHeader)
+
+/* ==================== 5. SHOW SCROLL UP ==================== */
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up');
+    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); 
+    else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp)
+window.addEventListener('load', scrollUp)
+
+/* ==================== 6. INISIALISASI AOS ==================== */
+AOS.init({
+    duration: 1200,
+    once: true,
+});
